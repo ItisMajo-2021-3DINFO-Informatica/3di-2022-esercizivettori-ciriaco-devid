cfd44ee59a142ef60d348e8d2d12519063f5aed2
ciriaco-devid <101391891+ciriaco-devid@users.noreply.github.com>
2022-03-11 09:59:15 +0100

    progetto nuovo

---
+++ ciriacovettori1/ciriacovettori1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ciriacovettori1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eserccizio vettori 1°");
+
+        }
+    }
+}
