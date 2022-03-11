6b17cf91f5ad8b1136de6d80ee422e0d036eebb1
ciriaco-devid <101391891+ciriaco-devid@users.noreply.github.com>
2022-03-11 10:46:21 +0100

    esercizio vettori

--- ciriacovettori1/ciriacovettori1/Program.cs
+++ ciriacovettori1/ciriacovettori1/Program.cs
@@ -6,8 +6,27 @@ namespace ciriacovettori1
     {
         static void Main(string[] args)
         {
+            /*Crea una app che carica un vettore di 400 elementi con valori casuali compresi tra 1 e 100 e stampi il contenuto del vettore*/
+            
             Console.WriteLine("eserccizio vettori 1°");
 
+            const int dimensione = 400;
+            int[] vettore = new int[dimensione];
+            
+
+
+            Random generatore = new Random();
+            
+            for (int i =0; i < dimensione; i++)
+            {
+                vettore[i] = generatore.Next(1, 101);
+            }
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                Console.WriteLine(vettore[i]);
+            }
         }
+
     }
 }
